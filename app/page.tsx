ba238ef5a75944b4b6d5b030881c87379f81431a
CHANWOO LEE <chw210601@gmail.com>
2023-11-01 17:17:00 +0900

    Revert "Revert "블로그 포스트 정렬 순서를 slug 내림차 순으로 변경""
    This reverts commit a62f21e8d9c35a5fe7fbccb9788f04a8295e2bde.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
+import { /*sortPosts,*/ allCoreContent } from 'pliny/utils/contentlayer'
+import { sortPostsBySlug } from 'lib/utils/comparator'
 import { allBlogs } from 'contentlayer/generated'
 import Main from './Main'
 
 export default async function Page() {
-  const sortedPosts = sortPosts(allBlogs)
+  const sortedPosts = sortPostsBySlug(allBlogs)
   const posts = allCoreContent(sortedPosts)
   return <Main posts={posts} />
 }
