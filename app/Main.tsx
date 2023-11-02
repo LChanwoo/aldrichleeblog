1b4888cb314ac0c88e471c1aea7fc23586373e6e
CHANWOO LEE <chw210601@gmail.com>
2023-11-02 10:12:20 +0900

    태그 색상 변경, 포스트 제목에 hover 시 미묘한 색상 변경 기능 추가

--- app/Main.tsx
+++ app/Main.tsx
@@ -38,7 +38,7 @@ export default function Home({ posts }) {
                           <h2 className="text-2xl font-bold leading-8 tracking-tight">
                             <Link
                               href={`/blog/${slug}`}
-                              className="text-gray-900 dark:text-gray-100"
+                              className="text-gray-900 dark:text-gray-100 hover:text-gray-700 dark:hover:text-gray-300"
                             >
                               {title}
                             </Link>
--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -135,7 +135,7 @@ export default function ListLayoutWithTags({
                       <div className="space-y-3">
                         <div>
                           <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                            <Link href={`/${path}`} className="text-gray-900 dark:text-gray-100">
+                            <Link href={`/${path}`} className="text-gray-900 dark:text-gray-100 hover:text-gray-700 dark:hover:text-gray-300">
                               {title}
                             </Link>
                           </h2>
