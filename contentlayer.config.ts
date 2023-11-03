a5c351b3d0d665da955ea851e411a60b7b585412
CHANWOO LEE <chw210601@gmail.com>
2023-11-03 14:08:46 +0900

    블로그 만들기 포스트 추가

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -62,6 +62,23 @@ function createTagCount(allBlogs) {
   writeFileSync('./app/tag-data.json', JSON.stringify(tagCount))
 }
 
+function createCategoryCount(allBlogs) {  
+  const categoryCount: Record<string, number> = {}
+  allBlogs.forEach((file) => {
+    if (file.category && (!isProduction || file.draft !== true)) {
+
+        const formattedCategory = file.category
+        if (formattedCategory in categoryCount) {
+          categoryCount[formattedCategory] += 1
+        } else {
+          categoryCount[formattedCategory] = 1
+        }
+      }
+    }
+  )
+  writeFileSync('./app/category-data.json', JSON.stringify(categoryCount))
+}
+
 function createSearchIndex(allBlogs) {
   if (
     siteMetadata?.search?.provider === 'kbar' &&
@@ -153,6 +170,7 @@ export default makeSource({
   onSuccess: async (importData) => {
     const { allBlogs } = await importData()
     createTagCount(allBlogs)
+    createCategoryCount(allBlogs)
     createSearchIndex(allBlogs)
   },
 })
