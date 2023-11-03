0cc5ebd893581ab14e7c6478cce4a22628ae5547
CHANWOO LEE <chw210601@gmail.com>
2023-11-03 18:30:36 +0900

    code format 맞춤

--- app/categories/[categories]/page.tsx
+++ app/categories/[categories]/page.tsx
@@ -7,7 +7,11 @@ import categoryData from 'app/category-data.json'
 import { genPageMetadata } from 'app/seo'
 import { Metadata } from 'next'
 
-export async function generateMetadata({ params }: { params: { category: string } }): Promise<Metadata> {
+export async function generateMetadata({
+  params,
+}: {
+  params: { category: string }
+}): Promise<Metadata> {
   const category = decodeURI(params.category)
   return genPageMetadata({
     title: category,
@@ -24,28 +28,15 @@ export async function generateMetadata({ params }: { params: { category: string 
 export const generateStaticParams = async () => {
   const categoryCounts = categoryData as Record<string, number>
   const categoryKeys = Object.keys(categoryCounts)
-  const paths = categoryKeys.map((category) => {
-    // console.log("sd",category);
-    // if(category.includes('-')){category = category.split('-').join(' ')}
-    return ({
-      category: category,
-    })
-  })
+  const paths = categoryKeys.map((category) => ({ category: category }))
   return paths
 }
 
 export default function categoryPage({ params }: { params: { categories: string } }) {
-  console.log(params);
   const category = decodeURI(params.categories).split('-').join(' ')
-  console.log(category);
   const title = category[0].toUpperCase() + category.split(' ').join(' ').slice(1)
-  console.log(category);
   const filteredPosts = allCoreContent(
-    sortPosts(allBlogs.filter((post) => {
-      console.log(post.category ,"==", category);
-
-      return post.category == category
-    }))
+    sortPosts(allBlogs.filter((post) => post.category == category))
   )
   return <ListLayout posts={filteredPosts} title={title} />
 }
